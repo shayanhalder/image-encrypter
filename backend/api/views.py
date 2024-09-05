7b20cbba7dfee4ac76594e9471a390968946795b
Shayan Halder <halders2@uci.edu>
2024-09-04 18:15:59 -0700

    finish upload file data pipeline

--- backend/api/views.py
+++ backend/api/views.py
@@ -7,33 +7,25 @@ from django.http import FileResponse
 from .serializers import UserSerializer, NoteSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Note
+from django.http import JsonResponse
+from . import cdn
 
 # Create your views here.
 @api_view(['POST'])
 def encrypt_image_message(request):
-    print('got image!')
+    print('got request...')
+    
     file = request.FILES.get('file')
+    # print('file: ', file)
+    # print('file chunks: ', file.chunks())
     
     if file:
         if file.content_type == "image/jpeg":
-            with open('test_file.jpg', 'wb') as save_file:
-                for chunk in file.chunks():
-                    save_file.write(chunk)
+            response = cdn.upload_file(file, host_path='uploaded_images/test.jpg')
+            print('cdn response: ', response)
             
-            with open('test_file.jpg', 'a+b') as save_file:
-                save_file.write(b'hello world!')
-                image_data = save_file.read()
-            
-            return HttpResponse(image_data, content_type='image/jpeg', status=200) # "File uploaded successfully", status=200)
-        
-        
-            # with open('test_file.jpg', 'rb') as save_file:
-            #     data = save_file.read()
-            #     start = data.index(bytes.fromhex('FFD9'))
-                
-            #     save_file.seek(start + 2)
-            #     save_file.write(b'Hello world!')
-                
+            return JsonResponse(response) 
+             
                 
                 
         
