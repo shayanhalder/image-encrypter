45c7e0a2d09e085246883a1b206abd4b00a139e1
Shayan Halder <halders2@uci.edu>
2024-09-08 18:19:53 -0700

    use env variables

--- backend/api/cdn.py
+++ backend/api/cdn.py
@@ -1,17 +1,21 @@
 import requests
 import json
 from django.core.files.uploadedfile import InMemoryUploadedFile
+from dotenv import load_dotenv
+import os
 
-BEARER_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnRJZCI6IkZLdmtjamZOUDJjaXBLaTdCcDhmazd0a0VpViIsImNsaWVudE5hbWUiOiJpbWFnZS1lbmNyeXB0ZXIiLCJzY29wZSI6WyJhY2NvdW50OnJlYWQiLCJhY2NvdW50OndyaXRlIiwidXNlcjpyZWFkIiwidXNlcjp3cml0ZSIsImJpbGxpbmc6cmVhZCIsImJpbGxpbmc6d3JpdGUiLCJmaWxlczpyZWFkIiwiZmlsZXM6d3JpdGUiLCJmaWxlczpjcmVhdGUiLCJmaWxlczp1cGxvYWQ6bXVsdGlwYXJ0IiwiZmlsZXM6c2hhcmVkQmlsbGluZyIsInZpZGVvcyIsImltYWdlcyJdLCJpYXQiOjE3MjU0MzAyMDIsImV4cCI6MTcyNTQzMTQwMiwiYXVkIjoiYzNrM2VoancxdGhpdm5za3ptaWhwMzE1dG95bHQ1enQifQ.rcDt9PqgFufEnW8df2srH9gSew3V0hEpRTgeFFMCVpI'
+
+load_dotenv()
 
 # First, get a token
 
-TOKEN_URL = 'https://api.sirv.com/v2/token'
-UPLOAD_URL = 'https://api.sirv.com/v2/files/upload'
-STAT_URL = 'https://api.sirv.com/v2/files/stat'
-CDN_URL = 'https://image-encrypter.sirv.com'
-CLIENT_ID = 'FKvkcjfNP2cipKi7Bp8fk7tkEiV'
-CLIENT_SECRET = 'td+IqHPUUptndGwvneT0drcQ0DpeIBEOd2roxgmwPG6NdQfRtXd4g2VGJXogMwfOLnxgYxFWxz+LdFH9trG4GA=='
+TOKEN_URL = os.getenv("TOKEN_URL") #'https://api.sirv.com/v2/token'
+UPLOAD_URL = os.getenv("UPLOAD_URL")#'https://api.sirv.com/v2/files/upload'
+STAT_URL = os.getenv("STAT_URL")#'https://api.sirv.com/v2/files/stat'
+CDN_URL = os.getenv("CDN_URL")#'https://image-encrypter.sirv.com'
+CLIENT_ID = os.getenv("CLIENT_ID") #'FKvkcjfNP2cipKi7Bp8fk7tkEiV'
+CLIENT_SECRET = os.getenv("CLIENT_SECRET") #'td+IqHPUUptndGwvneT0drcQ0DpeIBEOd2roxgmwPG6NdQfRtXd4g2VGJXogMwfOLnxgYxFWxz+LdFH9trG4GA=='
+
 
 def get_token():
     payload = {
